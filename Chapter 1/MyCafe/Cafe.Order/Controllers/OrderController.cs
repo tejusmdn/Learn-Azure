8b202e5ab83381dbe31491bcb5b92ada058c2ab3
Tejus <tejusmdn@gmail.com>
2022-12-07 13:58:54 +1000

    Implemented a small cafe service that returns a list of drinks available to order.

---
+++ Chapter 1/MyCafe/Cafe.Order/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cafe.Order.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        /// <summary>
+        /// Get a list of drinks that are available to order.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var drinks = new[] { "Cappuccino", "Mocha", "Flat White", "Espresso", "Hot Chocolate" };
+            return Ok(drinks);
+        }
+    }
+}
